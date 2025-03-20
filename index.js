a982332d16bd2a729db44e793e34bc0e9cde076a
priyanshu <priyanshuharrdy@gmail.com>
2025-03-20 12:21:34 +0530

    added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
+const cors = require('cors'); 
 const supplierRoutes = require('./routes/supplierRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
@@ -9,6 +9,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors()); 
 
 // Routes
 app.use('/suppliers', supplierRoutes);
